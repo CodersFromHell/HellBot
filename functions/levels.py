ba25cd5a898b650b0a1365469aa2cbfbf565f156
Alexander Burton <amacburton@gmail.com>
2016-10-24 11:29:20 -0500

    kinda made stuff for levels

--- functions/levels.py
+++ functions/levels.py
@@ -1,1 +1,9 @@
 import discord
+
+class Level:
+	levels = {} # user (discord.Member) => level object (Level)
+
+	def getLevel(user): # returns level Object
+		pass
+	def checkForLevelUp(user): # checks for level up on message sent
+		pass
