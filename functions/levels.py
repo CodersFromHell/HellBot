339c47b1a05cee9ce7df824288bc39492eae0a28
Alexander Burton <amacburton@gmail.com>
2016-10-25 13:05:31 -0500

    Added basic leveling system

--- functions/levels.py
+++ functions/levels.py
@@ -1,9 +1,18 @@
 import discord
+import user
 
-class Level:
-	levels = {} # user (discord.Member) => level object (Level)
+class Level()
+	def __init__(self, user : CustomUser):
+		self.parent = user
+		self.level = 0
+		self.xp = 0
+		# this is used so the object can refer to the user that has this object
 
-	def getLevel(user): # returns level Object
-		pass
-	def checkForLevelUp(user): # checks for level up on message sent
+	@staticmethod
+	# gets the total amount of XP required to level up from level n-1 to level n
+	def getXpToLevelUpTo(level):
+		return 100 + 20*(level-1)
+		# we can change this later
+
+	def checkForLevelUp(): # checks for level up, should be called on message sent
 		pass
