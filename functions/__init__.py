3d580720b378c79e4c1179d3994eeb6a0d001fc7
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-10-27 18:25:46 +0200

    Added __init__.py to use import *

---
+++ functions/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+Hell Bot
+~~~~~~~~~~~~~~~~~~~
+import all Functions by *
+"""
+
+from .admintools import Mute
+from .cookies import Cookie
+from .latex import Placeholder
+from .levels import Level
+from .music import Music
+from .notifications import Notifications
+from .user import CustomUser
