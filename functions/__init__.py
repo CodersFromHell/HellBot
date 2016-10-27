b79ec7b9bb9d08e6900f1c5a4e9d603fcef41d20
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-10-27 18:46:01 +0200

    Fixed __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -8,8 +8,5 @@ import all Functions by *
 
 from .admintools import Mute
 from .cookies import Cookie
-from .latex import Placeholder
 from .levels import Level
-from .music import Music
-from .notifications import Notifications
 from .user import CustomUser
