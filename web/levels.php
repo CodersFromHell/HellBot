b8a68a1a7db7de147e267b3e99586f93bae0baf8
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-10-28 05:27:21 +0200

    PHP System

--- web/levels.php
+++ web/levels.php
@@ -2,7 +2,6 @@
 <html lang=en>
 <head>
     <link rel="stylesheet" type="text/css" href="/css/root.css">
-    <link rel="stylesheet" type="text/css" href="/css/nav.css">
     <link rel="icon" href="/img/favicon.ico">
     <link rel="apple-touch-icon" href="/img/favicon.jpg">
     <link rel="apple-touch-icon-precomposed" href="/img/favicon.jpg">
@@ -11,6 +10,7 @@
 </head>
 <header>
     <?php
+    $active = 1;
     include '/utils/.nav.php'
     ?>
 </header>
@@ -18,18 +18,23 @@
 <div id="level-list">
     <table>
         <tbody>
-        <tr>
-            <td>Username</td>
-            <td>10XP</td>
-            <td>50XP</td>
-            <td>#10</td>
-        </tr>
-        <tr>
-            <td>Username</td>
-            <td>5XP</td>
-            <td>50XP</td>
-            <td>#11</td>
-        </tr>
+        <?php
+        $json = json_decode(file_get_contents("conf/levels.json"), true);
+        foreach($json as $user){
+        $username = $user['name'];
+        $xp = $user['xp'];
+        $reXp = 50 + 5 * ($user['lvl'] - 1);
+        $rank = 1;
+        echo "
+        <tr class='card'>
+            <td>$username</td>
+            <td>$xp XP</td>
+            <td>-</td>
+            <td>$reXp XP</td>
+            <td>$rank</td>
+        </tr>";
+        }
+        ?>
         </tbody>
     </table>
 </div>
@@ -42,8 +47,8 @@
             users[i]['id'] = keys[i];
             users[i]['xp'] = json[keys[i]]['xp'];
             users[i]['of'] = 50 + 5 * (json[keys[i]]['lvl'] - 1);
-            users[i]['lvl'] = json[keys[i]]['lvl']
-            users[i]['name'] = json[keys[i]]['name']
+            users[i]['lvl'] = json[keys[i]]['lvl'];
+            users[i]['name'] = json[keys[i]]['name'];
         }
         console.log(users)
     });
