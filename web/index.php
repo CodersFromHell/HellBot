b7cf899265a49d5852ffe11c80b9994c69cc29d4
vlt. Mondanzo <sackboysteve1@gmail.com>
2016-10-28 03:27:44 +0200

    Bump web folder

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/root.css">
+    <link rel="stylesheet" type="text/css" href="/css/nav.css">
+    <link rel="icon" href="/img/favicon.ico">
+    <link rel="apple-touch-icon" href="/img/favicon.jpg">
+    <link rel="apple-touch-icon-precomposed" href="/img/favicon.jpg">
+    <link rel="apple-touch-startup-image" href="/img/load.jpg">
+</head>
+<header>
+    <?php
+    include '/utils/.nav.php'
+    ?>
+
+</header>
+</html>
---
+++ web/levels.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/root.css">
+    <link rel="stylesheet" type="text/css" href="/css/nav.css">
+    <link rel="icon" href="/img/favicon.ico">
+    <link rel="apple-touch-icon" href="/img/favicon.jpg">
+    <link rel="apple-touch-icon-precomposed" href="/img/favicon.jpg">
+    <link rel="apple-touch-startup-image" href="/img/load.jpg">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<header>
+    <?php
+    include '/utils/.nav.php'
+    ?>
+</header>
+<body>
+<div id="level-list">
+    <table>
+        <tbody>
+        <tr>
+            <td>Username</td>
+            <td>10XP</td>
+            <td>50XP</td>
+            <td>#10</td>
+        </tr>
+        <tr>
+            <td>Username</td>
+            <td>5XP</td>
+            <td>50XP</td>
+            <td>#11</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
